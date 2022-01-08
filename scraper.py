1c5eed7140c31f32148c82fd375bb8d96584df58
jburky15 <jburky15@gmail.com>
2022-01-08 15:40:07 -0500

    Fixed a small search error

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import requests
 import re
 
 # Get the compenent the user is looking for from all pages of the given site
-pc_component = input("Enter the GPU you want to find: ")
+pc_component = input("Enter the product you want to find: ")
 
 url = f"https://www.newegg.com/p/pl?d={pc_component}&N=4131"
 page = requests.get(url).text
@@ -31,15 +31,19 @@ for page in range(1, pages + 1):
 
         link = parent['href']
         parent_price = item.find_parent(class_="item-container")
-        price = parent_price.find(class_="price-current").strong.string
-
-        items_found[item] = {"price": int(
-            price.replace(",", "")), "link": link}
+        try:
+            price = parent_price.find(
+                class_="price-current").find("strong").string
+            items_found[item] = {"price": int(
+                price.replace(",", "")), "link": link}
+        except:
+            pass
 
 # Sort all found products by price and print
-sort_items = sorted(items_found.items(), lambda x: x[1]['price'])
+sort_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
 
 for item in sort_items:
     print(item[0])
-    print(f"${items[1]['price']}")
-    print(items[1]['link'])
+    print(f"${item[1]['price']}")
+    print(item[1]['link'])
+    print("")
