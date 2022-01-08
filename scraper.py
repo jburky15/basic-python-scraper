6b2426891b1e0140b3ebb6d49b67a9cb19f0987d
jburky15 <jburky15@gmail.com>
2022-01-08 12:21:34 -0500

    Updated scraper

--- scraper.py
+++ scraper.py
@@ -1,18 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = "https://www.newegg.com/amd-ryzen-7-5800x/p/N82E16819113665?quicklink=true"
+# Get the compenent the user is looking for from all pages of the given site
+pc_component = input("Enter the GPU you want to find: ")
 
-results = requests.get(url)
-doc = BeautifulSoup(results.text, "html.parser")
+url = f"https://www.newegg.com/p/pl?d={pc_component}&N=4131"
+page = requests.get(url).text
+doc = BeautifulSoup(page, "html.parser")
 
-product = doc.find_all("h1")
-product_parent = product[1].parent
-product_heading = product_parent.find(class_="product-title")
+page_number = doc.find(class_="list-tool-pagination-text").strong
+pages = int(str(page_number).split("/")[-2].split(">")[-1][:-1])
 
-prices = doc.find_all(text="$")
-parent = prices[0].parent
-strong = parent.find("strong")
+items_found = {}
 
-for heading in product_heading:
-    print(f"Current price of", heading.strip(), ": $", strong.string)
+# Get links and pricing for all of the given compenent that are currently available
+for page in range(1, pages + 1):
+    url = f"https://www.newegg.com/p/pl?d={pc_component}&N=4131&page={page}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+    inner_content = doc.find(
+        class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    items = inner_content.find_all(text=re.compile(pc_component))
+
+    for item in items:
+        parent = item.parent
+
+        if parent.name != "a":
+            continue
+
+        link = parent['href']
+        parent_price = item.find_parent(class_="item-container")
+        price = parent_price.find(class_="price-current").strong.string
+
+        items_found[item] = {"price": int(
+            price.replace(",", "")), "link": link}
+
+# Sort all found products by price and print
+sort_items = sorted(items_found.items(), lambda x: x[1]['price'])
+
+for item in sort_items:
+    print(item[0])
+    print(f"${items[1]['price']}")
+    print(items[1]['link'])
